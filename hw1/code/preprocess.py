29ca9d9bb9e1e90b167790bdbd69434245bceadd
Logan Heft <61875329+hefty99@users.noreply.github.com>
2022-02-16 22:01:24 -0500

    Update preprocess.py

--- hw1/code/preprocess.py
+++ hw1/code/preprocess.py
@@ -8,12 +8,15 @@ def get_data(inputs_file_path='/Users/loganheft/Downloads/data/train-images.gz',
 	normalization = 1/255.0
 	offset16 = 16
 	offset8 = 8
-
+	image_size = 28
+	
 	with open(inputs_file_path, 'rb') as f, gzip.GzipFile(fileobj=f) as bytestream:
-		file = bytestream.read()
-		inputs_arr= normalization * np.frombuffer(file,dtype=np.uint8, count=-1, offset=offset16)
-		inputs_arr= np.float32(np.reshape(inputs_arr, (num_examples, image_size **2)))
-
+		file_content = bytestream.read()
+		inputs_array = (1/255.0) * np.frombuffer(file_content,
+						dtype=np.uint8, count=-1, offset=offset16)
+		inputs_array = np.reshape(
+			inputs_array, (num_examples, image_size * image_size))
+	
 	with open(labels_file_path, 'rb') as g, gzip.GzipFile(fileobj=g) as bytestream:
 		file = bytestream.read()
 		labels_arr = np.frombuffer(file, dtype=np.uint8, count=-1, offset=offset8)
